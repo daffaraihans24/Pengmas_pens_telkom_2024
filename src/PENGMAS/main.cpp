2cb58b8c45af01bebb535da6b4941efefbe1f40b
daffaraihans24 <daffaraihansharto@gmail.com>
2024-08-03 14:57:51 +0700

    Add rainfall sensor

--- src/PENGMAS/main.cpp
+++ src/PENGMAS/main.cpp
@@ -5,42 +5,34 @@
 #include <ModbusRTUMaster.h>
 #include <time.h>
 #include <HardwareSerial.h>
-#include <ESP_Google_Sheet_Client.h>
-#include <GS_SDHelper.h>
 
 //RS485 pin definition
-const uint8_t DERE_PIN = 4;
-
+const uint8_t dePin = 4;
 HardwareSerial mySerial(1);
 const uint8_t RXpin = 16;
 const uint8_t TXpin = 17;
 
 // WiFi Declare
-String wifiSSID = "YOUR SSID ID";
-String wifiPassword = "YOUR SSID PASSWORD";
+String wifiSSID = "YOUR SSID";
+String wifiPassword = "YOUR PASSWORD";
 
 // NTP Configuration
 const char* ntpServer = "pool.ntp.org";
 const long gmtOffset_sec = 6 * 3600;  //WIB Time, Indonesia
 const int daylightOffset_sec = 3600;
 
-// Google Project ID
-#define PROJECT_ID "YOUR PROJECT ID"
-// Service Account's client email
-#define CLIENT_EMAIL "YOUR CLIENT EMAIL ID"
-// Service Account's private key
-const char PRIVATE_KEY[] PROGMEM = "-----BEGIN PRIVATE KEY-----\n-----END PRIVATE KEY-----\n";
-
-// The ID of the spreadsheet where you'll publish the data
-const char spreadsheetId[] = "YOUR SPREADSHEET ID";
-
+// Supabase API credentials
+const char* supabaseUrl = "yoursupabase url";
+const char* supabaseKey = "your api key";
+String endpoint = "your end point";
 
 // Making modbus object
-ModbusRTUMaster modbus(mySerial, DERE_PIN);
-uint16_t holdingRegisterWindSpeed [1] = {0};
-uint16_t holdingRegisterWindDirection [1] = {0};
-uint16_t holdingRegisterSolarRadiation [1]= {0};
-uint16_t holdingRegisterClimate [3]= {0,0,0};
+ModbusRTUMaster modbus(mySerial, dePin);
+uint16_t holdingRegisterWindSpeed [1] = {0};    // The data read from the wind speed register
+uint16_t holdingRegisterWindDirection [1] = {0}; // The data read from the wind direction register
+uint16_t holdingRegisterSolarRadiation [1]= {0}; // The data read from the solar radiation register
+uint16_t holdingRegisterClimate [3]= {0,0,0}; // The data read from the climate registers
+uint16_t holdingRegisterRainfall [2]= {0,0}; // The data read from the rainfall registers
 
 float windSpeed;
 float windDirection;
@@ -48,20 +40,21 @@ float solarRadiation;
 float temperature;
 float humidity;
 float pressure;
+float rainfall;
 
 // Function Declare
 void postHTTP();
-void postToGoogleSheets();
-void tokenStatusCallback(TokenInfo info);
 void readWindSpeed();
 void readWindDirection();
 void readSolarRadiation();
 void readClimateData();
+void readRainfall();
 void connectWifi();
 void AutoReconnectWiFi();
 void syncLocalTime();
-String getFormattedDate();
 void processError();
+String getFormattedDate();
+
 
 //Setting interval time
 unsigned long previousMillis = 0;
@@ -69,13 +62,10 @@ const long interval = 1000;
 
 void setup() {
   Serial.begin(9600);
-  modbus.begin(9600, SERIAL_8N1, RXpin, TXpin);
+  modbus.begin(9600, SERIAL_8N1, RXpin, TXpin); // baudrate, config, RX, TX
   modbus.setTimeout(1000);
   connectWifi();
   syncLocalTime();
-  GSheet.begin(CLIENT_EMAIL, PROJECT_ID, PRIVATE_KEY);
-  GSheet.setTokenCallback(tokenStatusCallback);
-  GSheet.setPrerefreshSeconds(10 * 60);
 }
 
 void loop() {
@@ -91,14 +81,15 @@ void loop() {
     delay(300);
     readClimateData();
     delay(300);
-    postToGoogleSheets();
-    //postHTTP();
+    readRainfall();
+    delay(300);
+    postHTTP();
   }
 }
 
 void postHTTP() {
   Serial.println("Sending data sensors...");
-  String url = "https://yourapi/api/sensor";
+  String url = String(supabaseUrl) + endpoint;
   HTTPClient http;
   String response;
 
@@ -116,7 +107,7 @@ void postHTTP() {
 
   serializeJson(buff, jsonParams);
   http.begin(url);
-  http.addHeader("Content-Type", "application/json");
+  http.addHeader("apikey", supabaseKey);
   int statusCode = http.POST(jsonParams);
   response = http.getString();
 
@@ -125,57 +116,18 @@ void postHTTP() {
 
   } else {
     Serial.println("Post Method Failed!");
+    Serial.println("HTTP Response code: " + String(statusCode));
+    Serial.println("Response: " + response);
   }
-}
-
-void postToGoogleSheets() {
-  Serial.println("Sending data sensors to Google Sheets...");
-
-  FirebaseJson response;
-  FirebaseJson valueRange;
-
-  // Get timestamp
-  String formattedDate = getFormattedDate();
-
-  valueRange.add("majorDimension", "COLUMNS");
-  valueRange.set("values/[0]/[0]", formattedDate);
-  valueRange.set("values/[1]/[0]", windSpeed);
-  valueRange.set("values/[2]/[0]", windDirection);
-  valueRange.set("values/[3]/[0]", solarRadiation);
-  valueRange.set("values/[4]/[0]", temperature);
-  valueRange.set("values/[5]/[0]", humidity);
-  valueRange.set("values/[6]/[0]", pressure);
-  
-  // For Google Sheet API ref doc, go to https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets.values/append
-  // Append values to the spreadsheet
-  bool success = GSheet.values.append(&response /* returned response */, spreadsheetId /* spreadsheet Id to append */, "Sheet1!A1" /* range to append */, &valueRange /* data range to append */);
-  if (success){
-    response.toString(Serial, true);
-    valueRange.clear();
-    Serial.println("Post Method Success!");
-  }
-  else{
-    Serial.println(GSheet.errorReason());
-    Serial.println("Post Method Failed!");
-  }
-}
-
-void tokenStatusCallback(TokenInfo info){
-    if (info.status == token_status_error){
-        GSheet.printf("Token info: type = %s, status = %s\n", GSheet.getTokenType(info).c_str(), GSheet.getTokenStatus(info).c_str());
-        GSheet.printf("Token error: %s\n", GSheet.getTokenError(info).c_str());
-    }
-    else{
-        GSheet.printf("Token info: type = %s, status = %s\n", GSheet.getTokenType(info).c_str(), GSheet.getTokenStatus(info).c_str());
-    }
+  http.end();
 }
 
 // RK100-02 Sensor
 void readWindSpeed() {
- if(modbus.readHoldingRegisters(1, 0, holdingRegisterWindSpeed, 1)){
+ if(modbus.readHoldingRegisters(1, 0, holdingRegisterWindSpeed, 1)){  //readHoldingRegisters(ID Slave, startAddress, buffer, quantity)
     windSpeed = holdingRegisterWindSpeed[0]/10.0;
     Serial.print("Wind Speed: ");
-    Serial.print(windSpeed);
+    Serial.print(windSpeed, 2);
     Serial.println(" m/s");
  }
  else processError();
@@ -183,10 +135,10 @@ void readWindSpeed() {
 
 // RK110-02 Sensor
 void readWindDirection() {
-  if(modbus.readHoldingRegisters(2, 0, holdingRegisterWindDirection, 1)){
+  if(modbus.readHoldingRegisters(2, 0, holdingRegisterWindDirection, 1)){ //readHoldingRegisters(ID Slave, startAddress, buffer, quantity)
     windDirection = holdingRegisterWindDirection[0];
     Serial.print("Wind Direction: ");
-    Serial.print(windDirection);
+    Serial.print(windDirection, 2);
     Serial.println(" degree");
  }
  else processError();
@@ -194,10 +146,10 @@ void readWindDirection() {
 
 // RK200-04 Sensor
 void readSolarRadiation() {
-    if(modbus.readHoldingRegisters(6, 0, holdingRegisterSolarRadiation, 1)){
+    if(modbus.readHoldingRegisters(6, 0, holdingRegisterSolarRadiation, 1)){  //readHoldingRegisters(ID Slave, startAddress, buffer, quantity)
     solarRadiation = holdingRegisterSolarRadiation[0];
     Serial.print("Solar Radiation : ");
-    Serial.print(solarRadiation);
+    Serial.print(solarRadiation, 2);
     Serial.println(" W/m²");
  }
  else processError();
@@ -205,20 +157,31 @@ void readSolarRadiation() {
 
 // RK330-01 Sensor
 void readClimateData() {
-  if(modbus.readHoldingRegisters(3, 0, holdingRegisterClimate, 3)){
+  if(modbus.readHoldingRegisters(3, 0, holdingRegisterClimate, 3)){ //readHoldingRegisters(ID Slave, startAddress, buffer, quantity)
     temperature = holdingRegisterClimate[0]/10.0;
     humidity = holdingRegisterClimate[1]/10.0;
     pressure = holdingRegisterClimate[2]/10.0;
 
     Serial.print("Temperature: ");
-    Serial.print(temperature);
+    Serial.print(temperature, 2);
     Serial.println(" °C");
     Serial.print("Humidity: ");
-    Serial.print(humidity);
+    Serial.print(humidity, 2);
     Serial.println(" %");
     Serial.print("Pressure: ");
-    Serial.print(pressure);
+    Serial.print(pressure, 2);
     Serial.println(" mbar");
+ }
+ else processError();
+}
+
+// RK400-04 Sensor
+void readRainfall() {
+  if(modbus.readHoldingRegisters(4, 0, holdingRegisterRainfall, 2)){ //readHoldingRegisters(ID Slave, startAddress, buffer, quantity)
+    rainfall = holdingRegisterRainfall[1] / 10;
+    Serial.print("Rainfall : ");
+    Serial.print(rainfall, 2);
+    Serial.println(" mm");
  }
  else processError();
 }
